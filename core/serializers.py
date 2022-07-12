ae8ccb53105a7b4322ac71f8a080f3f47f4031a6
Joao Mateus <joao.amaral@ifam.edu.br>
2022-07-12 15:36:50 -0300

    capas doe serializer

--- core/serializers.py
+++ core/serializers.py
@@ -5,3 +5,9 @@ class EdicoesComemorativasSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.EdicoesComemorativas
         fields = '__all__'
+
+class CapasDoeSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = models.CapasDoe
+        fields = '__all__'
+
