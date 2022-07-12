ab27d28874ede9f9c2ca9eb35ac148b86bb13a1a
Joao Mateus <joao.amaral@ifam.edu.br>
2022-07-12 15:17:05 -0300

    image field edicoes comemorativas

--- core/models.py
+++ core/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 # Create your models here.
+def upload_image(instance, filename):
+    return f'{instance.codigo}-{filename}'
 
 class EdicoesComemorativas(models.Model):
     codigo = models.CharField(max_length=25)
     resumo = models.CharField(max_length=500)
     data = models.CharField(max_length=10)
     referencia = models.CharField(max_length=300)
+    image = models.ImageField(
+        upload_to='images/edicoes', blank=True, null=True)
 
     class Meta:
         managed = True
