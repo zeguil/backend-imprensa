7e59cd3d42d325e4fab072d6bb341c71e9bacc27
Joao Mateus <joao.amaral@ifam.edu.br>
2022-07-12 15:35:02 -0300

    capas doe

--- core/models.py
+++ core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-def upload_image(instance, filename):
-    return f'{instance.codigo}-{filename}'
 
 class EdicoesComemorativas(models.Model):
     codigo = models.CharField(max_length=25)
@@ -10,10 +8,27 @@ class EdicoesComemorativas(models.Model):
     data = models.CharField(max_length=10)
     referencia = models.CharField(max_length=300)
     image = models.ImageField(
-        upload_to='images/edicoes', blank=True, null=True)
+        upload_to='images/edicoes', blank=True, null=True
+    )
 
     class Meta:
         managed = True
         db_table = 'edicao_come'
         verbose_name = 'Edicao_Come'
         verbose_name_plural = 'Edicoes_Come'
+
+
+class CapasDoe(models.Model):
+    titulo = models.CharField(max_length=30)
+    ano = models.CharField(max_length=10)
+    resumo = models.CharField(max_length=250)
+    capa = models.ImageField(
+        upload_to='images/capas', blank=True, null=True
+    )
+
+    class Meta: 
+        managed = True
+        db_table = 'capa_doe'
+        verbose_name = 'Capa Doe'
+        verbose_name_plural = 'Capas_Doe'
+
